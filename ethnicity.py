48c53e4b7ef869e9ca174b089ff17ed7b779817a
paulsun096 <paulsun096@gmail.com>
2022-03-25 14:24:24 -0500

    lasped returning to returning

--- ethnicity.py
+++ ethnicity.py
@@ -9,8 +9,9 @@ from ethnicolr import census_ln, pred_census_ln
 def sakura_box():
     names = pd.read_csv('jan_2022_data.csv')
     df = pred_census_ln(names, 'SHIPPING_LAST_NAME')
-    
+    names['CUSTOMER_STATUS'] = names['CUSTOMER_STATUS'].apply(lambda x: x.replace('LAPSED_RETURNER', 'RETURNING'))
+
     print(df)
-    df.to_excel('jan_2022_data.xlsx')
+    df.to_excel('jan_2022_ethnicity.xlsx')
 
 sakura_box()
